86404e25061bd3c5bb44fd9b867f30568bebaaf7
Espada222 <espada2836@gmail.com>
2026-05-15 14:21:18 +0300

    исправление 1

--- DZ_Python_Счетчик обратного отсчета.py
+++ DZ_Python_Счетчик обратного отсчета.py
@@ -1,8 +1,15 @@
-num = int(input("Введите целое положительное число: "))
+while True:
+    try:
+        num = int(input("Введите число от 0 до 5: "))
 
-if num < 0:
-    print("Ошибка: Введите целое положительное число")
-else:
-    while num >= 0:
-        print(num)
-        num -= 1
+        if 0 <= num <= 5:
+            print(f"Обратный отсчёт от {num} до 0:")
+            while num >= 0:
+                print(num)
+                num -= 1
+            break
+        else:
+            print("Ошибка: число должно быть в диапазоне от 0 до 5. Попробуйте снова.")
+
+    except ValueError:
+        print("Ошибка: введите целое число. Попробуйте снова.")
